ce0ecfb28eb413faa52fa447d2275b4cb6b826e0
Liza <liza@MacBook-Air-Elizaveta.local>
2022-12-30 16:08:54 +0300

    f

--- src/mainpage/Banner/Banner.js
+++ src/mainpage/Banner/Banner.js
@@ -7,7 +7,7 @@ function Banner(props) {
 
         <div className="AppHeader flex justify-between items-center">
             <div>
-                <h1 className="AppHeader__title mb-2.5">Вместе мы справимся</h1>
+                <h1 className="AppHeader__title mb-2.5">Вместе мы справимся!</h1>
                 <p className="AppHeader__descr ">Найдите своего психолога онлайн</p>
 
                 <PurpleButton class={'AppHeader__button'} title={'Найти психолога'} ></PurpleButton>
